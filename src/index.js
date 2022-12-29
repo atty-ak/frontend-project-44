9a06fa8d13eeef47e74308e5017729a8500b2069
Atty-code <andrewkuzkin1@gmail.com>
2022-12-29 19:58:25 +0300

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const roundsCount = 3;
+
+export default (description, game) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(description);
+
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [correctQuestion, correctAnswer] = game(i);
+    console.log(`Question: ${correctQuestion}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
