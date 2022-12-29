9eb42bbba7b965999458756a92dc7f8bd262a242
Atty-code <andrewkuzkin1@gmail.com>
2022-12-29 20:00:29 +0300

    Add games/calc-game.js

---
+++ src/games/calc-game.js
@@ -0,0 +1,24 @@
+import index from '../index.js';
+
+const description = 'What is the result of the expression?';
+const getRoundGame = () => {
+  const randomFirstNum = Math.ceil(Math.random() * 20);
+  const randomSecondNum = Math.ceil(Math.random() * 10);
+  const availableOperators = ['+', '-', '*'];
+  const randomNumOperator = Math.floor(Math.random() * availableOperators.length);
+  const randomOperator = availableOperators[randomNumOperator];
+  let correctAnswer = 0;
+  if (randomOperator === '+') {
+    correctAnswer = randomFirstNum + randomSecondNum;
+  } else if (randomOperator === '-') {
+    correctAnswer = randomFirstNum - randomSecondNum;
+  } else {
+    correctAnswer = randomFirstNum * randomSecondNum;
+  }
+  const correctQuestion = `${randomFirstNum} ${randomOperator} ${randomSecondNum}`;
+  return [correctQuestion, String(correctAnswer)];
+};
+
+export default () => {
+  index(description, getRoundGame);
+};
